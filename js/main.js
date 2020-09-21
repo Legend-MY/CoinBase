22f3a53c9daf5579ee7ccd3ff90a0d3291b7a450
Legend-MY <markov2008@bigmir.net>
2020-09-21 17:25:07 +0300

    finished header and hero sections, made them responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const dropdown = document.querySelector('.header__btn');
 const dropdownItems = document.querySelector('.header__sell');
+const menuBurger = document.querySelector('.menu-burger');
+const navBar = document.querySelector('.nav');
+const wrapper = document.querySelector('.wrapper');
 
 dropdown.onclick = function () {
   dropdownItems.classList.toggle('header__sell_active');
+}
+
+menuBurger.onclick = function () {
+  menuBurger.classList.toggle('menu-burger-active');
+  navBar.classList.toggle('nav_active');
+  wrapper.classList.toggle('wrapper_active');
 }
